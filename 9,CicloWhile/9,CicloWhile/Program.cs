85edc9dfbddcfcf1829228fad57cf7c9f8af5d96
pabloandreszarate <158299580+pabloandreszarate@users.noreply.github.com>
2026-03-11 13:29:53 -0500

    Add files via upload

---
+++ 9,CicloWhile/9,CicloWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }/*int contador = 1;
+            int acumulador = 0;
+
+            while (contador != 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"contador:{contador}, acumulador:{acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine($"La sumade los cinco primeros numeros enteros es: {acumulador}");*/
+
+
+    }
+}
